c25196ae39c3667b9c4755416dad2407e9a66612
SamCooklin <sam.cooklin2@gmail.com>
2022-06-02 21:21:11 +0100

    L4 Change

--- Lesson4.py
+++ Lesson4.py
@@ -23,15 +23,41 @@ class Developer(Employee):
         super().__init__(first,last,pay)
         self.prog_lang = prog_lang
 
+class Manager(Employee):
+    def __init__(self, first, last, pay,employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+        
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+        
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.fullname())
+
 
 dev_1 = Developer("Corey" , "Schafer" , 50000 , "Python")
 dev_2 = Developer("Test" , "User" , 60000 , "Java")
 
-print(dev_1.prog_lang)
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
-print(dev_2.pay)
+mgr_1 = Manager("Sue" , "Smith" , 90000, [dev_1])
+print(mgr_1.email)
+return(mgr_1)
+
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+# print(dev_1.prog_lang)
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+# print(dev_2.pay)
 
 
 
